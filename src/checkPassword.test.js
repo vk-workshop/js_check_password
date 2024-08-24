54af2fbe2f90146befa86f98bffddbafca7063e5
Volodymyr Kolisnichenko <vk-workshop@vovalogic.local>
2024-08-24 12:04:46 +0200

    solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,33 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
+  it(`should return 'true' for the valid password`
+    + `with min 8 to 16 characters`, () => {
+    const result = checkPassword('Password1!');
 
+    expect(result).toBe(true);
   });
 
-  // write more tests here
+  it(`should accept only letters of the Latin alphabet Aa-Zz`, () => {
+    const result = checkPassword('Пароль123!');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for password`
+    + `with more than 16 characters`, () => {
+    const result = checkPassword('Password123456789!');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should contain at least`
+    + `1 digit, 1 special character, 1 uppercase letter.`, () => {
+    const result = checkPassword('Strong-password1');
+
+    expect(result).toBe(true);
+  });
 });
